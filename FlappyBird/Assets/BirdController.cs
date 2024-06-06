aa6b9a068aae85de75965791990f1308740a3848
Mateusz Pusty <mateusz.pusty@cdv.pl>
2024-06-06 14:27:09 +0200

    Added code

--- FlappyBird/Assets/BirdController.cs
+++ FlappyBird/Assets/BirdController.cs
@@ -7,19 +7,36 @@ public class BirdController : MonoBehaviour
     public Rigidbody2D rb2D;
     public Vector2 JumpForce;
 
+    public static bool GameOver;
+    public static bool HasStarted;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        GameOver = false;
+        HasStarted = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (GameOver)
+            return;
+
         if(Input.GetButtonDown("Jump"))
         {
+            if(HasStarted == false)
+            {
+                HasStarted = true;
+                rb2D.gravityScale = 1;
+            }
+
             rb2D.AddForce(JumpForce, ForceMode2D.Impulse);
         }
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameOver = true;
     }
 }
--- FlappyBird/Assets/MoveController.cs
+++ FlappyBird/Assets/MoveController.cs
@@ -6,15 +6,27 @@ public class MoveController : MonoBehaviour
 {
     public float Speed;
 
+    public float DespawnPointX;
+    public float SpawnPointX;
+
+    public float MinY;
+    public float MaxY;
+
     void Start()
     {
-        
+        transform.position = new Vector3(transform.position.x, Random.Range(MinY, MaxY), transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (BirdController.GameOver || BirdController.HasStarted == false)
+            return;
+
         transform.position = new Vector3(transform.position.x + Speed * Time.deltaTime,
-            transform.position.y, transform.position.z);    
+            transform.position.y, transform.position.z);
+
+        if (transform.position.x < DespawnPointX)
+            transform.position = new Vector3(SpawnPointX, Random.Range(MinY, MaxY), transform.position.z);
     }
 }
